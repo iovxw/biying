8f264e0ebfda8c99785cb5c637bbee8b7231e0e1
iovxw <iovxw@outlook.com>
2019-05-01 18:53:35 +0800

    Update i18n

--- assets/i18n/base.ts
+++ assets/i18n/base.ts
@@ -2,11 +2,153 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>SettingPage</name>
+    <message>
+        <location filename="../SettingPage.qml" line="39"/>
+        <source>Desktop Enviroment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="59"/>
+        <source>Command to set wallpaper:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="81"/>
+        <source>Automatically Change Wallpaper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="96"/>
+        <source>Enable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="106"/>
+        <source>Interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="119"/>
+        <source>minuts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="126"/>
+        <source>Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="135"/>
+        <source>Newest</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="141"/>
+        <location filename="../SettingPage.qml" line="192"/>
+        <source>Favourites</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="147"/>
+        <source>Random</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="155"/>
+        <source>Resolution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="162"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="171"/>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="183"/>
+        <source>Disk usage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="200"/>
+        <source>Others</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="210"/>
+        <source>Autoremove wallpapers from</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="223"/>
+        <source>days ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="230"/>
+        <source>Clear all other wallpapers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="237"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="244"/>
+        <source>Server: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="249"/>
+        <source>Client: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WallpaperFlow</name>
+    <message>
+        <location filename="../WallpaperFlow.qml" line="159"/>
+        <source>Set as Wallpaper</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
         <location filename="../main.qml" line="11"/>
         <source>Biying Wallpaper</source>
         <extracomment>Window title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="54"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="62"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="74"/>
+        <source>Favourites</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="77"/>
+        <source>Wallpapers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="80"/>
+        <source>Setting</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- assets/i18n/zh_CN.ts
+++ assets/i18n/zh_CN.ts
@@ -2,12 +2,154 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN" sourcelanguage="en">
 <context>
+    <name>SettingPage</name>
+    <message>
+        <location filename="../SettingPage.qml" line="39"/>
+        <source>Desktop Enviroment</source>
+        <translation>桌面环境</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="59"/>
+        <source>Command to set wallpaper:</source>
+        <translation>设置壁纸命令：</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="81"/>
+        <source>Automatically Change Wallpaper</source>
+        <translation>自动修改壁纸</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="96"/>
+        <source>Enable</source>
+        <translation>启用</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="106"/>
+        <source>Interval</source>
+        <translation>间隔</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="119"/>
+        <source>minuts</source>
+        <translation>分钟</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="126"/>
+        <source>Mode</source>
+        <translation>模式</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="135"/>
+        <source>Newest</source>
+        <translation>最新</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="141"/>
+        <location filename="../SettingPage.qml" line="192"/>
+        <source>Favourites</source>
+        <translation>喜爱</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="147"/>
+        <source>Random</source>
+        <translation>随机</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="155"/>
+        <source>Resolution</source>
+        <translation>分辨率</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="162"/>
+        <source>Preview</source>
+        <translation>预览</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="171"/>
+        <source>Download</source>
+        <translation>下载</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="183"/>
+        <source>Disk usage</source>
+        <translation>空间占用</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="200"/>
+        <source>Others</source>
+        <translation>其他</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="210"/>
+        <source>Autoremove wallpapers from</source>
+        <translation>自动删除</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="223"/>
+        <source>days ago</source>
+        <translation>天前的壁纸</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="230"/>
+        <source>Clear all other wallpapers</source>
+        <translation>立即清理非喜爱壁纸</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="237"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="244"/>
+        <source>Server: </source>
+        <translation>服务端： </translation>
+    </message>
+    <message>
+        <location filename="../SettingPage.qml" line="249"/>
+        <source>Client: </source>
+        <translation>客户端： </translation>
+    </message>
+</context>
+<context>
+    <name>WallpaperFlow</name>
+    <message>
+        <location filename="../WallpaperFlow.qml" line="159"/>
+        <source>Set as Wallpaper</source>
+        <translation>设为壁纸</translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
         <location filename="../main.qml" line="11"/>
         <source>Biying Wallpaper</source>
         <extracomment>Window title</extracomment>
         <translation>碧影壁纸</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="54"/>
+        <source>Open</source>
+        <translation>开启</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="62"/>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="74"/>
+        <source>Favourites</source>
+        <translation>喜爱</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="77"/>
+        <source>Wallpapers</source>
+        <translation>所有壁纸</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="80"/>
+        <source>Setting</source>
+        <translation>设置</translation>
     </message>
 </context>
 </TS>
