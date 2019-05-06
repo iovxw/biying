273a083695bf6ac185cefa7490fe1e3cb4e3ffc3
iovxw <iovxw@outlook.com>
2019-05-07 00:23:11 +0800

    Add a set wallpaper fallback from Variety
    fix #2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@
 #![feature(vec_remove_item)]
 #![recursion_limit = "128"]
 
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::os::unix::fs::PermissionsExt;
+
 use cpp::*;
 use qmetaobject::*;
 
@@ -42,11 +46,38 @@ qrc! { init_ressource,
 
 fn main() {
     init_ressource();
+
+    let set_wallpaper_fallback = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/assets/variety/data/scripts/set_wallpaper"
+    ));
+
     let mut engine = QmlEngine::new();
 
     let wallpapers = implementation::Wallpapers::new();
     let wallpapers = QObjectBox::new(wallpapers);
     let wallpapers = wallpapers.pinned();
+
+    {
+        let config = &wallpapers.borrow().config.borrow();
+        if !config.cache_dir.exists() {
+            fs::create_dir_all(&config.cache_dir).expect("create cache_dir");
+        }
+        let fallback_script_path = config.cache_dir.join("set_wallpaper");
+        let mut fallback_script =
+            File::create(&fallback_script_path).expect("create set_wallpaper");
+        fallback_script
+            .write_all(set_wallpaper_fallback)
+            .expect("write set_wallpaper");
+        fallback_script
+            .set_permissions(fs::Permissions::from_mode(0o755))
+            .expect("set 755 for set_wallpaper");
+        let mut de = config.de.borrow_mut();
+        let custom_cmd = &mut de.last_mut().unwrap().cmd;
+        if custom_cmd.to_slice().is_empty() {
+            *custom_cmd = format!("{} \"$WALLPAPER\" auto",&*fallback_script_path.to_string_lossy()).into();
+        }
+    }
 
     engine.set_object_property("wallpapers".into(), wallpapers);
 
@@ -66,5 +97,10 @@ fn main() {
     }
     engine.load_file("qrc:/assets/main.qml".into());
     engine.exec();
-    wallpapers.borrow().config.borrow().save().expect("Failed to save configs");
+    wallpapers
+        .borrow()
+        .config
+        .borrow()
+        .save()
+        .expect("Failed to save configs");
 }
