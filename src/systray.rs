230e7f2f4bc82df87f3b5c7f73c87beb5aed68a4
iovxw <iovxw@outlook.com>
2020-05-26 00:27:48 +0800

    Upgrade ksni to 0.1, and refactor the systray module

--- src/systray.rs
+++ src/systray.rs
@@ -1,26 +1,27 @@
 use std::sync::{mpsc, Mutex};
-use std::thread;
 
 use image::ImageFormat;
-use ksni::{self, menu, tray};
+use ksni;
 use qmetaobject::*;
 
 lazy_static! {
-    static ref TRAY_RX: Mutex<Option<mpsc::Receiver<Cmd>>> = Mutex::default();
-    static ref TRAY_TX: Mutex<Option<mpsc::Sender<Cmd>>> = Mutex::default();
-    static ref ON_OPEN: Mutex<Option<Box<dyn Fn() -> bool + Send>>> = Mutex::default();
-    static ref ON_QUIT: Mutex<Option<Box<dyn Fn() + Send>>> = Mutex::default();
+    /// Send Cmd to TrayProxy
+    static ref REMOTE_CMD_SENDER: Mutex<Option<Box<dyn Fn(Cmd) + Send>>> = Mutex::default();
+    /// If TrayProxy isn't running, all Cmd will be here
+    static ref LOCAL_CMD_RECIVER: Mutex<Option<mpsc::Receiver<Cmd>>> = Mutex::default();
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum Cmd {
     Open,
     Quit,
 }
 
 pub fn wait() -> Cmd {
-    let rx = TRAY_RX.lock().unwrap();
-    let rx = rx.as_ref().expect("");
+    let rx = LOCAL_CMD_RECIVER
+        .try_lock()
+        .expect("another thread is waiting");
+    let rx = rx.as_ref().expect("no tray is running");
     rx.recv().unwrap()
 }
 
@@ -35,51 +36,56 @@ pub struct TrayProxy {
 impl TrayProxy {
     fn connect_to_backend(&mut self) {
         let qptr = QPointer::from(&*self);
-        let on_open_callback = queued_callback(move |ret: mpsc::Sender<bool>| {
-            if let Some(ptr) = qptr.as_ref() {
-                ptr.open();
-            }
-            ret.send(qptr.as_ref().is_some()).unwrap();
-        });
-        *ON_OPEN.lock().unwrap() = Some(Box::new(move || {
-            let (tx, rx) = mpsc::channel();
-            (on_open_callback)(tx);
-            rx.recv().unwrap()
-        }));
-        let qptr = QPointer::from(&*self);
-        let on_quit_callback = queued_callback(move |_| {
-            if let Some(ptr) = qptr.as_ref() {
-                ptr.quit();
+        let on_open_callback = queued_callback(move |cmd| {
+            let this = qptr
+                .as_ref()
+                .unwrap_or_else(|| unreachable!("REMOTE_CMD_SENDER should have beed dropped"));
+            match cmd {
+                Cmd::Open => this.open(),
+                Cmd::Quit => this.quit(),
             }
         });
-        *ON_QUIT.lock().unwrap() = Some(Box::new(move || (on_quit_callback)(())));
+        *REMOTE_CMD_SENDER.lock().unwrap() = Some(Box::new(on_open_callback));
     }
 }
 
 impl Drop for TrayProxy {
     fn drop(&mut self) {
-        *ON_OPEN.lock().unwrap() = None;
-        *ON_QUIT.lock().unwrap() = None;
+        *REMOTE_CMD_SENDER.lock().unwrap() = None;
     }
 }
 
-pub struct Tray;
+pub struct Tray {
+    cmd_tx: mpsc::Sender<Cmd>,
+}
 
-impl ksni::Tray for Tray {
-    type Err = std::convert::Infallible;
-    fn tray_properties() -> tray::Properties {
-        tray::Properties {
-            icon_name: "livewallpaper-indicator".to_owned(),
-            icon_pixmap: icons(),
-            ..Default::default()
+impl Tray {
+    fn emit_cmd(&mut self, cmd: Cmd) {
+        let tray_proxy_exist = REMOTE_CMD_SENDER
+            .lock()
+            .unwrap()
+            .as_ref()
+            .map(|emit| (emit)(cmd))
+            .is_some();
+        if !tray_proxy_exist {
+            self.cmd_tx.send(cmd).unwrap()
         }
     }
-    fn menu() -> Vec<menu::MenuItem> {
-        use menu::*;
+}
+
+impl ksni::Tray for Tray {
+    fn icon_name(&self) -> String {
+        "livewallpaper-indicator".to_owned()
+    }
+    fn icon_pixmap(&self) -> Vec<ksni::Icon> {
+        icons()
+    }
+    fn menu(&self) -> Vec<ksni::MenuItem<Self>> {
+        use ksni::menu::*;
         vec![
             StandardItem {
                 label: "Open".into(),
-                activate: Box::new(open),
+                activate: Box::new(|this: &mut Self| this.emit_cmd(Cmd::Open)),
                 ..Default::default()
             }
             .into(),
@@ -87,7 +93,7 @@ impl ksni::Tray for Tray {
             StandardItem {
                 label: "Quit".into(),
                 icon_name: "application-exit".into(),
-                activate: Box::new(quit),
+                activate: Box::new(|this: &mut Self| this.emit_cmd(Cmd::Quit)),
                 ..Default::default()
             }
             .into(),
@@ -96,50 +102,18 @@ impl ksni::Tray for Tray {
 }
 
 pub fn run_tray_in_background() {
-    let mut rx = TRAY_RX.lock().unwrap();
-    if rx.is_none() {
+    let mut cmd_rx = LOCAL_CMD_RECIVER.lock().unwrap();
+    if cmd_rx.is_none() {
         let (sender, recver) = mpsc::channel();
-        *rx = Some(recver);
-        *TRAY_TX.lock().unwrap() = Some(sender);
-        thread::spawn(move || {
-            ksni::run(Tray);
-        });
-    }
-}
+        *cmd_rx = Some(recver);
 
-fn open() {
-    let arrived = ON_OPEN
-        .lock()
-        .unwrap()
-        .as_ref()
-        .map(|callback| (callback)())
-        .unwrap_or(false);
-    if !arrived {
-        TRAY_TX
-            .lock()
-            .unwrap()
-            .as_ref()
-            .unwrap()
-            .send(Cmd::Open)
-            .unwrap()
+        let service = ksni::TrayService::new(Tray { cmd_tx: sender });
+        service.spawn();
     }
-}
-
-fn quit() {
-    if let Some(callback) = &*ON_QUIT.lock().unwrap() {
-        (callback)();
-    }
-    TRAY_TX
-        .lock()
-        .unwrap()
-        .as_ref()
-        .unwrap()
-        .send(Cmd::Quit)
-        .unwrap();
 }
 
 // TODO: const fn
-fn icons() -> Vec<tray::Icon> {
+fn icons() -> Vec<ksni::Icon> {
     let images: &[&[u8]] = &[
         // include_bytes!("../assets/tray-icon_16.png"),
         include_bytes!("../assets/tray-icon_24.png"),
@@ -156,7 +130,7 @@ fn icons() -> Vec<tray::Icon> {
             // rgba rotated to argb
             pixel.0.rotate_right(1);
         }
-        let icon = tray::Icon {
+        let icon = ksni::Icon {
             width: width as i32,
             height: height as i32,
             data: img.into_raw(),
